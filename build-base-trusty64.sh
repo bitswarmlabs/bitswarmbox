3e829cc2a16ede845a2dfff7d6ef4087523267a5
Reuben Avery <ravery@bitswarm.io>
2016-06-23 09:00:26 -0400

    removing two unwanted scripts from index

--- build-base-trusty64.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-which bitswarmbox || gem install bitswarmbox
-
-bitswarmbox build vagrant \
-    --name=ubuntu-trusty64-base \
-    --template=ubuntu/trusty64 \
-    --provider=virtualbox \
-    --puppet
