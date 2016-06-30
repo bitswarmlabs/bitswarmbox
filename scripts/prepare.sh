106733efc12ff0daf62ec8a317fcfb069e2b0b28
Reuben Avery <ravery@bitswarm.io>
2016-06-29 21:22:48 -0400

    shoring up provisioning scripts

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -5,25 +5,28 @@ minimal_apt_get_install='apt-get install -y'
 
 set -e
 
+os_distro=$(lsb_release -is | tr '[:upper:]' '[:lower:]')
+os_release=$(lsb_release -cs)
+
 # update the apt cache and packages
-case $(lsb_release -cs) in
+case $os_release in
     'precise')
-        apt-get clean
-        rm -rf /var/lib/apt/lists/*
-        apt-get clean
+        apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys E1DD270288B4E6030699E45FA1715D88E1DF1F24
+        echo 'deb http://ppa.launchpad.net/git-core/ppa/ubuntu precise main' | sudo tee /etc/apt/sources.list.d/git.list
+    ;;
+    'trusty')
+        apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys E1DD270288B4E6030699E45FA1715D88E1DF1F24
+        echo 'deb http://ppa.launchpad.net/git-core/ppa/ubuntu trusty main' | sudo tee /etc/apt/sources.list.d/git.list
     ;;
     *)
     ;;
 esac
 
 apt-get -qy update
+
+$minimal_apt_get_install git wget curl
+
 apt-get -qy upgrade
-
-$minimal_apt_get_install zsh vim git wget curl
-
-echo "## zsh executable: `which zsh`"
-echo "## vim executable: `which vi`"
-echo "## git executable: `which git`"
 
 mkdir -p /usr/local/bin
 mkdir -p /usr/local/sbin
