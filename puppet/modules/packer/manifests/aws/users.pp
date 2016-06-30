9f074d963c913ea4e8478d9fb7415ea1c4b09612
Reuben Avery <ravery@bitswarm.io>
2016-06-29 21:23:12 -0400

    shoring up provisioning scripts

--- puppet/modules/packer/manifests/aws/users.pp
+++ puppet/modules/packer/manifests/aws/users.pp
@@ -2,22 +2,25 @@ class packer::aws::users {
   include '::packer::aws'
   $service_acct = $::packer::aws::local_service_acct_user
 
-  if $service_acct != 'ubuntu' {
+  if $service_acct != 'ubuntu' and str2bool($::packer::aws::manage_users) {
     user { $service_acct:
       ensure     => present,
       home       => "/home/${service_acct}",
       managehome => true,
     }
-    ->
-    sudo::sudoers { $service_acct:
-      ensure   => 'present',
-      users    => [$service_acct],
-      tags     => ['NOPASSWD'],
-      defaults => [ 'env_keep += "SSH_AUTH_SOCK"' ]
-    }
+    # ->
+    # sudo::sudoers { $service_acct:
+    #   ensure   => 'present',
+    #   users    => [$service_acct],
+    #   tags     => ['NOPASSWD'],
+    # }
   }
 
+  class { 'ohmyzsh::config': theme_hostname_slug => '%M' }
+
   # for multiple users in one shot and set their shell to zsh
-  ohmyzsh::install { ['root', $service_acct]: set_sh => true, disable_auto_update => true }
-  ohmyzsh::theme { ['root', $service_acct]: theme => 'dpoggi' }
+  ohmyzsh::install { 'root': set_sh => true, disable_auto_update => true }
+  ohmyzsh::install { $service_acct: set_sh => true, disable_update_prompt => true }
+  ohmyzsh::plugins { ['root', $service_acct]: }
+  ohmyzsh::theme { ['root', $service_acct]: }
 }
