8847800a59ed82331118c30ecab859ad08ca0b56
Reuben Avery <ravery@bitswarm.io>
2016-07-01 11:38:49 -0400

    more facts for puppet provisioning, debugging packer puppet code

--- puppet/modules/packer/manifests/vagrant.pp
+++ puppet/modules/packer/manifests/vagrant.pp
@@ -1,5 +1,5 @@
 class packer::vagrant(
-  $manage_user = 'true',
+  $manage_user = 'false',
   $ohymzsh = 'true',
   $manage_ssh = 'false',
   $public_rsa_key = 'AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ==',
@@ -17,17 +17,18 @@ class packer::vagrant(
       managehome => true,
       gid        => 'vagrant',
       groups     => [ 'vagrant', ],
-      shell      => '/bin/bash',
       require    => Group[ 'vagrant' ],
     }
+  }
 
-    if str2bool($ohymzsh) {
-      # for multiple users in one shot and set their shell to zsh
-      ohmyzsh::install { 'root': set_sh => true, disable_auto_update => true }
-      ohmyzsh::install { 'vagrant': set_sh => true, disable_update_prompt => true }
-      ohmyzsh::plugins { ['root', 'vagrant']: }
-      ohmyzsh::theme { ['root', 'vagrant']: }
-    }
+  if str2bool($ohymzsh) {
+    include ohmyzsh
+
+    # for multiple users in one shot and set their shell to zsh
+    ohmyzsh::install { 'root': set_sh => true, disable_auto_update => true }
+    ohmyzsh::install { 'vagrant': set_sh => true, disable_update_prompt => true }
+    ohmyzsh::plugins { ['root', 'vagrant']: }
+    ohmyzsh::theme { ['root', 'vagrant']: }
   }
 
   if str2bool($manage_ssh) {
