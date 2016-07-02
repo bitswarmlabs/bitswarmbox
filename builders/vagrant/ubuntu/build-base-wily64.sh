72bf9e52f690adab5359878fdb903759898b1de2
Reuben Avery <ravery@bitswarm.io>
2016-07-02 15:00:05 -0400

    removing ubuntu wily64 build script

--- builders/vagrant/ubuntu/build-base-wily64.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-which bitswarmbox || gem install bitswarmbox
-
-bitswarmbox build vagrant \
-    --name=ubuntu-wily64-base \
-    --template=ubuntu/wily64 \
-    --provider=vmware \
-    --puppet --bootstrap
