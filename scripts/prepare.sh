874aec36d1e56bdbd4874d14ed317154e8d81d3b
Reuben Avery <ravery@bitswarm.io>
2016-07-03 10:25:40 -0400

    purging and rebuilding /etc/apt/sources.list for ubuntu precise and trusty

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -8,13 +8,25 @@ set -e
 os_distro=$(lsb_release -is | tr '[:upper:]' '[:lower:]')
 os_release=$(lsb_release -cs)
 
+echo "## OS Distro: ${os_distro}  Release: ${os_release}"
+
 # update the apt cache and packages
 case $os_release in
     'precise')
+        mv /etc/apt/sources.list /etc/apt/sources.list.bak
+        touch /etc/apt/sources.list
+        apt-get update
+        mv /etc/apt/sources.list.bak /etc/apt/sources.list
+        sudo apt-get update
         apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys E1DD270288B4E6030699E45FA1715D88E1DF1F24
         echo 'deb http://ppa.launchpad.net/git-core/ppa/ubuntu precise main' | sudo tee /etc/apt/sources.list.d/git.list
     ;;
     'trusty')
+        mv /etc/apt/sources.list /etc/apt/sources.list.bak
+        touch /etc/apt/sources.list
+        apt-get update
+        mv /etc/apt/sources.list.bak /etc/apt/sources.list
+        sudo apt-get update
         apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys E1DD270288B4E6030699E45FA1715D88E1DF1F24
         echo 'deb http://ppa.launchpad.net/git-core/ppa/ubuntu trusty main' | sudo tee /etc/apt/sources.list.d/git.list
     ;;
