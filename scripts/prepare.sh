53b0fcd855aed33600933dcc1d4458f54ae56d06
Reuben Avery <ravery@bitswarm.io>
2016-07-13 11:03:40 -0400

    adding r10k sources back, fixing apt problem for jessie

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -17,7 +17,7 @@ case $os_release in
         touch /etc/apt/sources.list
         apt-get update
         mv /etc/apt/sources.list.bak /etc/apt/sources.list
-        sudo apt-get update
+        apt-get update
         apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys E1DD270288B4E6030699E45FA1715D88E1DF1F24
         echo 'deb http://ppa.launchpad.net/git-core/ppa/ubuntu precise main' | sudo tee /etc/apt/sources.list.d/git.list
     ;;
@@ -26,9 +26,18 @@ case $os_release in
         touch /etc/apt/sources.list
         apt-get update
         mv /etc/apt/sources.list.bak /etc/apt/sources.list
-        sudo apt-get update
+        apt-get update
         apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys E1DD270288B4E6030699E45FA1715D88E1DF1F24
         echo 'deb http://ppa.launchpad.net/git-core/ppa/ubuntu trusty main' | sudo tee /etc/apt/sources.list.d/git.list
+    ;;
+    'jessie')
+        # Fixing hash sum mismatch issues re https://gist.github.com/trastle/5722089 and
+        # http://stackoverflow.com/questions/15505775/debian-apt-packages-hash-sum-mismatch
+        echo 'Acquire::http::Pipeline-Depth "0";' > /etc/apt/apt.conf.d/99fixbadproxy
+        echo 'Acquire::http::No-Cache=True;' >>  /etc/apt/apt.conf.d/99fixbadproxy
+        echo 'Acquire::BrokenProxy=true;' >>  /etc/apt/apt.conf.d/99fixbadproxy
+        rm  -rf /var/lib/apt/lists/*
+        apt-get update -qy
     ;;
     *)
     ;;
