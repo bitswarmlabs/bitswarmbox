e97344ed08efed49e64bc4ec41e2e97076ebed1e
Reuben Avery <ravery@bitswarm.io>
2016-07-22 12:27:57 -0400

    running foreman-installer

--- scripts/foreman-post.sh
+++ scripts/foreman-post.sh
@@ -14,5 +14,13 @@ cat /etc/puppetlabs/code/bsl_bootstrap/hiera.yaml
 for f in $(find /etc/puppetlabs/code/bsl_bootstrap/hieradata -type f); do
     cat $f
 done
+set +v
+
+echo "## Disabling foreman and puppetserver services, they'll be re-enabled by bootstrap init scripts"
+set -v -e
+systemctl disable puppetserver
+systemctl disable foreman
+systemctl disable foreman-proxy
+systemctl disable apache2
 
 exit 0
